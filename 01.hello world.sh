8ce2b07d85171466593fbc8c825eaccb645b338b
sakila <sakilalakshmi95@gmail.com>
2023-08-18 21:10:25 +0530

    Hello World added

--- 01.hello world.sh
+++ 01.hello world.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 echo "Hello World,I am learning DevOps with AWS"
+PERSON1=Sakila
+PERSON2=Sownd
 
-echo "Sakila:Hi Sownd,good morning"
-echo "Sownd:Good Morning Sakila"
+echo "$PERSON1:Hi $PERSON2,good morning"
+echo "$PERSON2:Good Morning $PERSON1"
