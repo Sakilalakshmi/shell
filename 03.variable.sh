eb976074ae0015e45aa007914f549247be0a864c
sakila <sakilalakshmi95@gmail.com>
2023-08-19 11:40:43 +0530

    Hello World added

---
+++ 03.variable.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echo "Hello World,I am learning DevOps with AWS"
+PERSON1=$1 #this is the first value of command line
+PERSON2=$2 #this is the second value of command line
+
+echo "$PERSON1:Hi $PERSON2,good morning"
+echo "$PERSON2:Good Morning $PERSON1"
