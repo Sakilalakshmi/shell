6ef4b613ef86f645e74c0d20928200a373d1dbc4
sakila <sakilalakshmi95@gmail.com>
2023-08-20 21:59:41 +0530

    while loop implemented

--- 13.delete-logfiles.sh
+++ 13.delete-logfiles.sh
@@ -9,3 +9,9 @@ LOGFILE=$LOGSDIR/$SCRIPT_NAME-$DATE.logs
 FILES_TO_DELETE=$(find $APP_LOG_DIR -name "*.log" -type f -mtime +14)
 
 echo "$FILES_TO_DELETE"
+
+while read line
+do 
+    echo "Deleting $line" &> $LOGFILE
+    rm -rf $line
+done <<< $FILES_TO_DELETE
