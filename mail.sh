36f458bfa41558c9f8420bbe1d47f6f2076ef104
sakila <sakilalakshmi95@gmail.com>
2023-08-22 06:38:39 +0530

    added script

--- mail.sh
+++ mail.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 
-#anyone in your team use the script  with argument
+#anyone in your project can call this script with arguments
+# mail.sh TO_ADDRESS SUBJECT BODY TEAM_NAME ALERT_TYPE
 
 TO_ADDRESS=$1
 SUBJECT=$2
 BODY=$(sed -e 's/[]\/$*.^[]/\\&/g' <<< $3)
-echo "escape content: $BODY"
+echo "escaped content: $BODY"
 TEAM_NAME=$4
 ALERT_TYPE=$5
 
-#echo "all arg : $@"
+#echo "all args: $@"
 
 FINAL_BODY=$(sed -e "s/TEAM_NAME/$TEAM_NAME/g" -e "s/ALERT_TYPE/$ALERT_TYPE/g" -e "s/MESSAGE/$BODY/g" template.html)
 
-echo "$FINAL_BODY" | mail -s "$(echo -e "$SUBJECT\n content-Type: text/html")" $TO_ADDRESS
+echo "$FINAL_BODY" | mail -s "$(echo -e "$SUBJECT\nContent-Type: text/html")" "$TO_ADDRESS"
